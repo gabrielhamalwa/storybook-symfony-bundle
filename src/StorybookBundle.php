de551e830b7c7d9883a6480c75163f77260f7146
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-10 14:36:07 +0200

    Chore: remove FragmentHandlerPass compiler pass
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- src/StorybookBundle.php
+++ src/StorybookBundle.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Storybook\SymfonyBundle;
 
 use Storybook\SymfonyBundle\DependencyInjection\Compiler\AssetPipelinePass;
-use Storybook\SymfonyBundle\DependencyInjection\Compiler\FragmentHandlerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
@@ -16,7 +15,6 @@ final class StorybookBundle extends Bundle
         parent::build($container);
 
         $container->addCompilerPass(new AssetPipelinePass());
-        $container->addCompilerPass(new FragmentHandlerPass());
     }
 
     public function getPath(): string
