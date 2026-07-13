3b6c3b7612990a057e4f132ef9706d4e3bd0ac6e
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-13 11:08:20 +0200

    Test: add kernel integration tests for service registration and asset pipeline wiring
    Add KernelTest to verify StorybookController registration and asset pipeline dependency injection. Tests cover default NullAssetPipeline wiring and conditional PentatrionViteAssetPipeline registration based on configuration.
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

---
+++ tests/KernelTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Storybook\SymfonyBundle\Tests;
+
+use Pentatrion\ViteBundle\PentatrionViteBundle;
+use Storybook\SymfonyBundle\Asset\AssetPipelineInterface;
+use Storybook\SymfonyBundle\Asset\NullAssetPipeline;
+use Storybook\SymfonyBundle\Asset\PentatrionViteAssetPipeline;
+use Storybook\SymfonyBundle\Controller\StorybookController;
+use Storybook\SymfonyBundle\StorybookBundle;
+use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Bundle\TwigBundle\TwigBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\UX\TwigComponent\TwigComponentBundle;
+
+final class KernelTest extends KernelTestCase
+{
+    protected static function createKernel(array $options = []): Kernel
+    {
+        return new class('test', true) extends Kernel {
+            public function registerBundles(): iterable
+            {
+                return [
+                    new FrameworkBundle(),
+                    new TwigBundle(),
+                    new TwigComponentBundle(),
+                    new PentatrionViteBundle(),
+                    new StorybookBundle(),
+                ];
+            }
+
+            public function registerContainerConfiguration(LoaderInterface $loader): void
+            {
+                $loader->load(function ($container) {
+                    $container->loadFromExtension('framework', ['test' => true]);
+                    $container->loadFromExtension('twig', ['default_path' => '%kernel.project_dir%/templates']);
+                    $container->loadFromExtension('twig_component', ['defaults' => [], 'anonymous_template_directory' => 'components']);
+                    $container->loadFromExtension('storybook', [
+                        'asset_pipeline' => $container->getParameter('storybook.test_asset_pipeline') ?? 'none',
+                    ]);
+                });
+            }
+        };
+    }
+
+    public function testStorybookControllerIsRegistered(): void
+    {
+        self::bootKernel();
+        $container = self::getContainer();
+
+        self::assertTrue($container->has(StorybookController::class));
+        self::assertInstanceOf(StorybookController::class, $container->get(StorybookController::class));
+    }
+
+    public function testNullAssetPipelineIsWiredByDefault(): void
+    {
+        self::bootKernel();
+        $container = self::getContainer();
+
+        self::assertInstanceOf(NullAssetPipeline::class, $this->getPipeline($container->get(StorybookController::class)));
+    }
+
+    public function testPentatrionViteAssetPipelineIsWiredWhenConfigured(): void
+    {
+        self::bootKernel(['environment' => 'test', 'debug' => true]);
+        self::getContainer()->setParameter('storybook.test_asset_pipeline', 'pentatrion_vite');
+        self::bootKernel();
+
+        $container = self::getContainer();
+        self::assertInstanceOf(PentatrionViteAssetPipeline::class, $this->getPipeline($container->get(StorybookController::class)));
+    }
+
+    private function getPipeline(StorybookController $controller): AssetPipelineInterface
+    {
+        $reflection = new \ReflectionProperty($controller, 'assetPipeline');
+
+        return $reflection->getValue($controller);
+    }
+}
