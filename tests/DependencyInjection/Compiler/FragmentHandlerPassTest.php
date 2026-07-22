4b489844b34d2b7db6d03db8df819e92bc7dd0be
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 17:53:44 +0200

    test: convert FragmentHandlerPassTest from PHPUnit to Pest
    Convert FragmentHandlerPassTest from class-based PHPUnit test case to Pest functional test style. Replace testAliasesFragmentHandlerClassWhenServiceExists and testDoesNotAliasWhenServiceIsMissing methods with equivalent Pest test() functions. Remove namespace declaration and TestCase inheritance.
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- tests/DependencyInjection/Compiler/FragmentHandlerPassTest.php
+++ tests/DependencyInjection/Compiler/FragmentHandlerPassTest.php
@@ -1,33 +1,22 @@
 <?php
 
 declare(strict_types=1);
-
-namespace Storybook\SymfonyBundle\Tests\DependencyInjection\Compiler;
-
-use PHPUnit\Framework\TestCase;
 use Storybook\SymfonyBundle\DependencyInjection\Compiler\FragmentHandlerPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Fragment\FragmentHandler;
+test('aliases fragment handler class when service exists', function () {
+    $container = new ContainerBuilder();
+    $container->register('fragment.handler', FragmentHandler::class);
 
-final class FragmentHandlerPassTest extends TestCase
-{
-    public function testAliasesFragmentHandlerClassWhenServiceExists(): void
-    {
-        $container = new ContainerBuilder();
-        $container->register('fragment.handler', FragmentHandler::class);
+    (new FragmentHandlerPass())->process($container);
 
-        (new FragmentHandlerPass())->process($container);
+    self::assertTrue($container->hasAlias(FragmentHandler::class));
+    self::assertSame('fragment.handler', (string) $container->getAlias(FragmentHandler::class));
+});
+test('does not alias when service is missing', function () {
+    $container = new ContainerBuilder();
 
-        self::assertTrue($container->hasAlias(FragmentHandler::class));
-        self::assertSame('fragment.handler', (string) $container->getAlias(FragmentHandler::class));
-    }
+    (new FragmentHandlerPass())->process($container);
 
-    public function testDoesNotAliasWhenServiceIsMissing(): void
-    {
-        $container = new ContainerBuilder();
-
-        (new FragmentHandlerPass())->process($container);
-
-        self::assertFalse($container->hasAlias(FragmentHandler::class));
-    }
-}
+    self::assertFalse($container->hasAlias(FragmentHandler::class));
+});
