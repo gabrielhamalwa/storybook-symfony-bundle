b8e6a054e0581e65884e2ef629c424ccd3d03b22
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 17:53:28 +0200

    test: convert ConfigurationTest from PHPUnit to Pest
    Convert ConfigurationTest from class-based PHPUnit test case to Pest functional test style. Replace testDefaultConfiguration and testPentatrionVitePipelineCanBeConfigured methods with equivalent Pest test() functions. Remove namespace declaration and TestCase inheritance.
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- tests/DependencyInjection/ConfigurationTest.php
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -1,34 +1,23 @@
 <?php
 
 declare(strict_types=1);
-
-namespace Storybook\SymfonyBundle\Tests\DependencyInjection;
-
-use PHPUnit\Framework\TestCase;
 use Storybook\SymfonyBundle\DependencyInjection\Configuration;
 use Symfony\Component\Config\Definition\Processor;
+test('default configuration', function () {
+    $config = (new Processor())->processConfiguration(new Configuration(), []);
 
-final class ConfigurationTest extends TestCase
-{
-    public function testDefaultConfiguration(): void
-    {
-        $config = (new Processor())->processConfiguration(new Configuration(), []);
+    self::assertSame('auto', $config['asset_pipeline']);
+    self::assertSame('app', $config['entrypoint']);
+    self::assertSame([], $config['cors_allowed_origins']);
+});
+test('pentatrion vite pipeline can be configured', function () {
+    $config = (new Processor())->processConfiguration(new Configuration(), [
+        [
+            'asset_pipeline' => 'pentatrion_vite',
+            'entrypoint' => 'storybook',
+        ],
+    ]);
 
-        self::assertSame('auto', $config['asset_pipeline']);
-        self::assertSame('app', $config['entrypoint']);
-        self::assertSame([], $config['cors_allowed_origins']);
-    }
-
-    public function testPentatrionVitePipelineCanBeConfigured(): void
-    {
-        $config = (new Processor())->processConfiguration(new Configuration(), [
-            [
-                'asset_pipeline' => 'pentatrion_vite',
-                'entrypoint' => 'storybook',
-            ],
-        ]);
-
-        self::assertSame('pentatrion_vite', $config['asset_pipeline']);
-        self::assertSame('storybook', $config['entrypoint']);
-    }
-}
+    self::assertSame('pentatrion_vite', $config['asset_pipeline']);
+    self::assertSame('storybook', $config['entrypoint']);
+});
