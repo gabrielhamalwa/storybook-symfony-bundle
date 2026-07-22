00edd5be1f8e91a15de5097a4bc79a948a19336e
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 16:19:33 +0200

    feat: register ComponentIndexer and StaticFormDataListener services with dependency injection configuration
    Add ComponentIndexer service with project directory parameter and ComponentIndexerInterface alias. Register StaticFormDataListener with high priority (2048) for kernel.request event. Add cors_allowed_origins parameter to CorsListener configuration.
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- src/Resources/config/services.php
+++ src/Resources/config/services.php
@@ -9,6 +9,9 @@ use Storybook\SymfonyBundle\Component\TemplateAdapter;
 use Storybook\SymfonyBundle\Component\TwigComponentAdapter;
 use Storybook\SymfonyBundle\Controller\StorybookController;
 use Storybook\SymfonyBundle\EventListener\CorsListener;
+use Storybook\SymfonyBundle\EventListener\StaticFormDataListener;
+use Storybook\SymfonyBundle\Indexer\ComponentIndexer;
+use Storybook\SymfonyBundle\Indexer\ComponentIndexerInterface;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $configurator): void {
@@ -33,11 +36,23 @@ return static function (ContainerConfigurator $configurator): void {
         ->set(ComponentResolver::class);
 
     $services
+        ->set(ComponentIndexer::class)
+        ->arg('$projectDir', '%kernel.project_dir%');
+
+    $services
+        ->alias(ComponentIndexerInterface::class, ComponentIndexer::class);
+
+    $services
         ->set(StorybookController::class)
         ->public()
         ->tag('controller.service_arguments');
 
     $services
         ->set(CorsListener::class)
+        ->arg('$allowedOrigins', '%storybook.cors_allowed_origins%')
         ->tag('kernel.event_listener', ['event' => 'kernel.response']);
+
+    $services
+        ->set(StaticFormDataListener::class)
+        ->tag('kernel.event_listener', ['event' => 'kernel.request', 'priority' => 2048]);
 };
