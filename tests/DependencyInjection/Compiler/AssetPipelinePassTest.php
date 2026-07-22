7a40c314f853028705a44a4bd06b8749117a1be8
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 17:53:37 +0200

    test: convert AssetPipelinePassTest from PHPUnit to Pest
    Convert AssetPipelinePassTest from class-based PHPUnit test case to Pest functional test style. Replace testDetectsPentatrionVite, testDetectsEncore, testDetectsAssetMapper, testFallsBackToNull, and testDoesNothingForExplicitPipeline methods with equivalent Pest test() functions. Convert helper methods createContainer and assertServiceIs to standalone functions. Replace PHPUnit assertions with Pest expect() syntax. Remove namespace declaration
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- tests/DependencyInjection/Compiler/AssetPipelinePassTest.php
+++ tests/DependencyInjection/Compiler/AssetPipelinePassTest.php
@@ -1,10 +1,6 @@
 <?php
 
 declare(strict_types=1);
-
-namespace Storybook\SymfonyBundle\Tests\DependencyInjection\Compiler;
-
-use PHPUnit\Framework\TestCase;
 use Pentatrion\ViteBundle\Service\EntrypointsLookupCollection;
 use Storybook\SymfonyBundle\Asset\AssetExtractorInterface;
 use Storybook\SymfonyBundle\Asset\AssetMapperPipeline;
@@ -17,71 +13,56 @@ use Storybook\SymfonyBundle\DependencyInjection\StorybookExtension;
 use Symfony\Component\AssetMapper\ImportMap\ImportMapGenerator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupCollection;
+test('detects pentatrion vite', function () {
+    $container = createContainer('auto');
+    $container->register(EntrypointsLookupCollection::class, EntrypointsLookupCollection::class);
 
-final class AssetPipelinePassTest extends TestCase
+    (new AssetPipelinePass())->process($container);
+
+    assertServiceIs(PentatrionViteAssetPipeline::class, $container);
+});
+test('detects encore', function () {
+    $container = createContainer('auto');
+    $container->register('webpack_encore.entrypoint_lookup_collection', EntrypointLookupCollection::class);
+
+    (new AssetPipelinePass())->process($container);
+
+    assertServiceIs(EncoreAssetPipeline::class, $container);
+});
+test('detects asset mapper', function () {
+    $container = createContainer('auto');
+    $container->register('asset_mapper.importmap.generator', ImportMapGenerator::class);
+
+    (new AssetPipelinePass())->process($container);
+
+    assertServiceIs(AssetMapperPipeline::class, $container);
+});
+test('falls back to null', function () {
+    $container = createContainer('auto');
+
+    (new AssetPipelinePass())->process($container);
+
+    assertServiceIs(NullAssetPipeline::class, $container);
+});
+test('does nothing for explicit pipeline', function () {
+    $container = createContainer('none');
+    $container->setDefinition(AssetExtractorInterface::class, new \Symfony\Component\DependencyInjection\Definition(NullAssetPipeline::class));
+
+    (new AssetPipelinePass())->process($container);
+
+    assertServiceIs(NullAssetPipeline::class, $container);
+});
+function createContainer(string $pipeline): ContainerBuilder
 {
-    public function testDetectsPentatrionVite(): void
-    {
-        $container = $this->createContainer('auto');
-        $container->register(EntrypointsLookupCollection::class, EntrypointsLookupCollection::class);
+    $container = new ContainerBuilder();
+    $container->setParameter('storybook.asset_pipeline', $pipeline);
+    $container->setParameter('storybook.entrypoint', 'app');
+    $container->setAlias(AssetPipelineInterface::class, AssetExtractorInterface::class);
 
-        (new AssetPipelinePass())->process($container);
-
-        $this->assertServiceIs(PentatrionViteAssetPipeline::class, $container);
-    }
-
-    public function testDetectsEncore(): void
-    {
-        $container = $this->createContainer('auto');
-        $container->register('webpack_encore.entrypoint_lookup_collection', EntrypointLookupCollection::class);
-
-        (new AssetPipelinePass())->process($container);
-
-        $this->assertServiceIs(EncoreAssetPipeline::class, $container);
-    }
-
-    public function testDetectsAssetMapper(): void
-    {
-        $container = $this->createContainer('auto');
-        $container->register('asset_mapper.importmap.generator', ImportMapGenerator::class);
-
-        (new AssetPipelinePass())->process($container);
-
-        $this->assertServiceIs(AssetMapperPipeline::class, $container);
-    }
-
-    public function testFallsBackToNull(): void
-    {
-        $container = $this->createContainer('auto');
-
-        (new AssetPipelinePass())->process($container);
-
-        $this->assertServiceIs(NullAssetPipeline::class, $container);
-    }
-
-    public function testDoesNothingForExplicitPipeline(): void
-    {
-        $container = $this->createContainer('none');
-        $container->setDefinition(AssetExtractorInterface::class, new \Symfony\Component\DependencyInjection\Definition(NullAssetPipeline::class));
-
-        (new AssetPipelinePass())->process($container);
-
-        $this->assertServiceIs(NullAssetPipeline::class, $container);
-    }
-
-    private function createContainer(string $pipeline): ContainerBuilder
-    {
-        $container = new ContainerBuilder();
-        $container->setParameter('storybook.asset_pipeline', $pipeline);
-        $container->setParameter('storybook.entrypoint', 'app');
-        $container->setAlias(AssetPipelineInterface::class, AssetExtractorInterface::class);
-
-        return $container;
-    }
-
-    private function assertServiceIs(string $class, ContainerBuilder $container): void
-    {
-        $definition = $container->getDefinition(AssetExtractorInterface::class);
-        $this->assertSame($class, $definition->getClass());
-    }
+    return $container;
+}
+function assertServiceIs(string $class, ContainerBuilder $container): void
+{
+    $definition = $container->getDefinition(AssetExtractorInterface::class);
+    expect($definition->getClass())->toBe($class);
 }
