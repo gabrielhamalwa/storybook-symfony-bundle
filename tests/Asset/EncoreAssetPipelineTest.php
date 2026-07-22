5effc89f0a9beddcf088053360024702292c9417
gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>
2026-07-22 17:52:02 +0200

    test: convert EncoreAssetPipelineTest from PHPUnit to Pest
    Convert EncoreAssetPipelineTest from class-based PHPUnit test case to Pest functional test style. Replace testReturnsEmptyAssetsWhenEntrypointThrows, testReturnsNormalizedAssetsForEntrypoint, and testExtractReturnsTypedCollection methods with equivalent Pest test() functions. Remove namespace declaration and TestCase inheritance.
    
    Signed-off-by: gabrielhamalwa <75419495+gabrielhamalwa@users.noreply.github.com>

--- tests/Asset/EncoreAssetPipelineTest.php
+++ tests/Asset/EncoreAssetPipelineTest.php
@@ -1,57 +1,44 @@
 <?php
 
 declare(strict_types=1);
-
-namespace Storybook\SymfonyBundle\Tests\Asset;
-
-use PHPUnit\Framework\TestCase;
 use Storybook\SymfonyBundle\Asset\EncoreAssetPipeline;
 use Storybook\SymfonyBundle\Dto\AssetCollection;
 use Storybook\SymfonyBundle\Dto\AssetScript;
 use Storybook\SymfonyBundle\Dto\AssetStyle;
 use Symfony\WebpackEncoreBundle\Asset\EntrypointLookupInterface;
+test('returns empty assets when entrypoint throws', function () {
+    $lookup = $this->createMock(EntrypointLookupInterface::class);
+    $lookup->method('getCssFiles')->willThrowException(new \RuntimeException('missing'));
 
-final class EncoreAssetPipelineTest extends TestCase
-{
-    public function testReturnsEmptyAssetsWhenEntrypointThrows(): void
-    {
-        $lookup = $this->createMock(EntrypointLookupInterface::class);
-        $lookup->method('getCssFiles')->willThrowException(new \RuntimeException('missing'));
+    $pipeline = new EncoreAssetPipeline($lookup, 'app');
 
-        $pipeline = new EncoreAssetPipeline($lookup, 'app');
+    self::assertSame([
+        'pipeline' => 'webpack-encore',
+        'styles' => [],
+        'scripts' => [],
+    ], $pipeline->getAssets());
+});
+test('returns normalized assets for entrypoint', function () {
+    $lookup = $this->createMock(EntrypointLookupInterface::class);
+    $lookup->method('getCssFiles')->with('app')->willReturn(['/build/app.css']);
+    $lookup->method('getJavaScriptFiles')->with('app')->willReturn(['/build/app.js']);
 
-        self::assertSame([
-            'pipeline' => 'webpack-encore',
-            'styles' => [],
-            'scripts' => [],
-        ], $pipeline->getAssets());
-    }
+    $pipeline = new EncoreAssetPipeline($lookup, 'app');
 
-    public function testReturnsNormalizedAssetsForEntrypoint(): void
-    {
-        $lookup = $this->createMock(EntrypointLookupInterface::class);
-        $lookup->method('getCssFiles')->with('app')->willReturn(['/build/app.css']);
-        $lookup->method('getJavaScriptFiles')->with('app')->willReturn(['/build/app.js']);
+    self::assertSame([
+        'pipeline' => 'webpack-encore',
+        'styles' => [['url' => '/build/app.css']],
+        'scripts' => [['url' => '/build/app.js', 'type' => 'module']],
+    ], $pipeline->getAssets());
+});
+test('extract returns typed collection', function () {
+    $lookup = $this->createMock(EntrypointLookupInterface::class);
+    $lookup->method('getCssFiles')->with('storybook')->willReturn(['/build/storybook.css']);
+    $lookup->method('getJavaScriptFiles')->with('storybook')->willReturn(['/build/storybook.js']);
 
-        $pipeline = new EncoreAssetPipeline($lookup, 'app');
+    $collection = (new EncoreAssetPipeline($lookup, 'storybook'))->extract();
 
-        self::assertSame([
-            'pipeline' => 'webpack-encore',
-            'styles' => [['url' => '/build/app.css']],
-            'scripts' => [['url' => '/build/app.js', 'type' => 'module']],
-        ], $pipeline->getAssets());
-    }
-
-    public function testExtractReturnsTypedCollection(): void
-    {
-        $lookup = $this->createMock(EntrypointLookupInterface::class);
-        $lookup->method('getCssFiles')->with('storybook')->willReturn(['/build/storybook.css']);
-        $lookup->method('getJavaScriptFiles')->with('storybook')->willReturn(['/build/storybook.js']);
-
-        $collection = (new EncoreAssetPipeline($lookup, 'storybook'))->extract();
-
-        self::assertSame('webpack-encore', $collection->pipeline);
-        self::assertEquals([new AssetStyle('/build/storybook.css', 'webpack-encore')], $collection->styles);
-        self::assertEquals([new AssetScript('/build/storybook.js', 'module', 'webpack-encore')], $collection->scripts);
-    }
-}
+    self::assertSame('webpack-encore', $collection->pipeline);
+    self::assertEquals([new AssetStyle('/build/storybook.css', 'webpack-encore')], $collection->styles);
+    self::assertEquals([new AssetScript('/build/storybook.js', 'module', 'webpack-encore')], $collection->scripts);
+});
